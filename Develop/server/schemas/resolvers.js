9edacc6415b1442bc4f15bb7c92a3c9a88405989
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-03-06 11:34:44 -0800

    resolvers file is finished

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,64 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const user = await User.findById(context.user._id).populate('savedBooks');
+        return user;
+      }
+      throw new Error('Not logged in!');
+    },
+    user: async (parent, { username }) => {
+      const user = await User.findOne({ username }).populate('savedBooks');
+      if (!user) {
+        throw new Error("Can't find user with that username!");
+      }
+      return user;
+    },
+  },
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new Error("Can't find user with that email!");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new Error('Incorrect password!');
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book } },
+          { new: true, runValidators: true }
+        ).populate('savedBooks');
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+        return updatedUser;
+      }
+      throw new Error('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
