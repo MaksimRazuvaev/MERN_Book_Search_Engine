1bb7a838dc471ec98ea83bad58eddefb23416eaa
Maksim Razuvaev <maksim.razuvaev.wa@gmail.com>
2023-03-06 13:32:33 -0800

    queries file is filled up

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+// This query will fetch the current logged-in user's information, including their id, username, email, bookCount, and savedBooks array.
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
